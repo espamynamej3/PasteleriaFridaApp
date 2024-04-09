0ed839e4b77f2b05d3fc0f877d0f2379689cfec5
espamynamej3 <espamynamej3@gmail.com>
2024-04-08 23:36:31 -0400

    Agregando el proyecto

---
+++ app/src/main/java/com/fridabardepostres/fridabardepostres/PasteleriaActivity.kt
@@ -0,0 +1,20 @@
+package com.fridabardepostres.fridabardepostres
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PasteleriaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.pasteleria_activity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
